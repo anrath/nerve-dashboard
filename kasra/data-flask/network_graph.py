5fddca51f42db3b209859eff34beba26f2b6d882
Anrath <aklounts@gmail.com>
2023-04-30 12:06:02 -0400

    improving network graph script efficiency

--- kasra/data-flask/network_graph.py
+++ kasra/data-flask/network_graph.py
@@ -4,145 +4,159 @@ import json
 import os
 import requests
 
-def create_ssid_graph(sub_path):
 
-    if sub_path == 'realtime':
+def net_exist(ap):
+    ssid = ""
+    try:
+        ssid = ap["dot11.device.responded_ssid_map"][0]["dot11.advertisedssid.ssid"]
+    except:
+        try:
+            ssid = ap["dot11.device.advertised_ssid_map"][0]["dot11.advertisedssid.ssid"]
+        except:
+            try:
+                ssid = ap["dot11.device.probed_ssid_map"][0]["dot11.probedssid.ssid"]
+            except:
+                ssid = "eduroam"
+    return ssid
+
+
+def create_ssid_graph(sub_path):
+    if sub_path == "realtime":
         user_password = "http://sniffer:sniffer@"
         server_ip = "172.26.99.45:2501/"
 
         ap_endpoint = "/devices/views/phydot11_accesspoints/devices.json"
-        ap_response = requests.get(user_password + server_ip + ap_endpoint, headers={"KISMET": "E62F6C667B3CF269798AC58E0D811D85"})
+        ap_response = requests.get(
+            user_password + server_ip + ap_endpoint, headers={"KISMET": "E62F6C667B3CF269798AC58E0D811D85"}
+        )
         ap_json = json.dumps(ap_response.json(), indent=4)
 
         wlan_endpoint = "/devices/views/phy-IEEE802.11/devices.json"
-        wlan_response = requests.get(user_password + server_ip + wlan_endpoint, headers={"KISMET": "E62F6C667B3CF269798AC58E0D811D85"})
+        wlan_response = requests.get(
+            user_password + server_ip + wlan_endpoint, headers={"KISMET": "E62F6C667B3CF269798AC58E0D811D85"}
+        )
         wlan_json = json.dumps(wlan_response.json(), indent=4)
 
         ssid_endpoint = "/phy/phy80211/ssids/views/ssids.json"
-        ssid_response = requests.get(user_password + server_ip + ssid_endpoint, headers={"KISMET": "E62F6C667B3CF269798AC58E0D811D85"})
+        ssid_response = requests.get(
+            user_password + server_ip + ssid_endpoint, headers={"KISMET": "E62F6C667B3CF269798AC58E0D811D85"}
+        )
         ssid_json = json.dumps(ssid_response.json(), indent=4)
 
-        with open(f'{DATA_PATH}/realtime/phydot11_accesspoints.json', "w") as outfile:
+        with open(f"{DATA_PATH}/realtime/phydot11_accesspoints.json", "w") as outfile:
             outfile.write(ap_json)
 
-        with open(f'{DATA_PATH}/realtime/phy-IEEE802.11.json', "w") as outfile:
+        with open(f"{DATA_PATH}/realtime/phy-IEEE802.11.json", "w") as outfile:
             outfile.write(wlan_json)
-            
-        with open(f'{DATA_PATH}/realtime/phy_phy80211_ssids_views_ssids.json', "w") as outfile:
+
+        with open(f"{DATA_PATH}/realtime/phy_phy80211_ssids_views_ssids.json", "w") as outfile:
             outfile.write(ssid_json)
 
-
     # Opening JSON files
-    with open(f'{DATA_PATH}/{sub_path}/phydot11_accesspoints.json', 'r') as openfile:
+    with open(f"{DATA_PATH}/{sub_path}/phydot11_accesspoints.json", "r") as openfile:
         aps = json.load(openfile)
 
-    with open(f'{DATA_PATH}/{sub_path}/phy_phy80211_ssids_views_ssids.json', 'r') as openfile:
+    with open(f"{DATA_PATH}/{sub_path}/phy_phy80211_ssids_views_ssids.json", "r") as openfile:
         ssids = json.load(openfile)
 
-    with open(f'{DATA_PATH}/{sub_path}/phy-IEEE802.11.json', 'r') as openfile:
-        wlan = json.load(openfile)
+    # with open(f"{DATA_PATH}/{sub_path}/phy-IEEE802.11.json", "r") as openfile:
+    #     wlan = json.load(openfile)
 
-    aps_df = pd.DataFrame()
-
-    for ap in aps:
-        try:
-            ssid = ap['dot11.device']['dot11.device.responded_ssid_map'][0]['dot11.advertisedssid.ssid']
-        except:
-            try:
-                ssid = ap['dot11.device']['dot11.device.advertised_ssid_map'][0]['dot11.advertisedssid.ssid']
-            except:
-                try:
-                    ssid = ap['dot11.device']['dot11.device.probed_ssid_map'][0]['dot11.probedssid.ssid']
-                except:
-                    ssid = 'eduroam'
-        ap_data = {
-            "key": [ap['kismet.device.base.key']], 
-            "manuf": [ap['kismet.device.base.manuf']], 
-            "macaddr": [ap['kismet.device.base.macaddr']],
-            "ssid": [ssid]
-        }
-        
-        aps_df = pd.concat([aps_df, pd.DataFrame(ap_data)], ignore_index=True)
+    aps_df = pd.read_json(f"{DATA_PATH}/{sub_path}/phydot11_accesspoints.json", orient="records")
+    aps_df = aps_df[
+        ["kismet.device.base.key", "kismet.device.base.manuf", "kismet.device.base.macaddr", "dot11.device"]
+    ]
+    aps_df.rename(
+        columns={
+            "kismet.device.base.key": "key",
+            "kismet.device.base.manuf": "manuf",
+            "kismet.device.base.macaddr": "macaddr",
+            "dot11.device": "ssid",
+        },
+        inplace=True,
+    )
+    aps_df["ssid"] = aps_df.apply(lambda row: net_exist(row["ssid"]), axis=1)
 
     # Attempt to fill in missing SSIDs
     for ssid in ssids:
-        set1 = set(aps_df[aps_df['ssid']=='']['key'].values)
-        list2 = ssid['dot11.ssidgroup.responding_devices']
+        set1 = set(aps_df[aps_df["ssid"] == ""]["key"].values)
+        list2 = ssid["dot11.ssidgroup.responding_devices"]
         common = list(set1.intersection(list2))
         for val in common:
-            ind = aps_df[aps_df['key']==val].index[0]
+            ind = aps_df[aps_df["key"] == val].index[0]
             # print(ind)
-            aps_df.at[ind, 'ssid'] = ssid['dot11.ssidgroup.ssid']
+            aps_df.at[ind, "ssid"] = ssid["dot11.ssidgroup.ssid"]
 
     # Remove empty SSIDs
-    aps_df = aps_df[aps_df['ssid']!='']
+    aps_df = aps_df[aps_df["ssid"] != ""]
 
-    wlan_df = pd.DataFrame()
-
-    for device in wlan:
-
-        wlan_data = {
-            "ap": [device['dot11.device']['dot11.device.last_bssid']],
-            "key": [device['kismet.device.base.key']], 
-            "manuf": [device['kismet.device.base.manuf']], 
-            "macaddr": [device['kismet.device.base.macaddr']],
-            # "ssid": [ssid],
-        }
-        
-        wlan_data = pd.DataFrame(wlan_data)
-
-        wlan_df = pd.concat([wlan_df, wlan_data], ignore_index=True)
-    wlan_df['ap_bool'] = False
+    wlan_df = pd.read_json(f"{DATA_PATH}/{sub_path}/phy-IEEE802.11.json", orient="records")
+    wlan_df = wlan_df[
+        ["dot11.device", "kismet.device.base.key", "kismet.device.base.manuf", "kismet.device.base.macaddr"]
+    ]
+    wlan_df.rename(
+        columns={
+            "dot11.device": "ap",
+            "kismet.device.base.key": "key",
+            "kismet.device.base.manuf": "manuf",
+            "kismet.device.base.macaddr": "macaddr",
+        },
+        inplace=True,
+    )
+    wlan_df["ap"] = wlan_df.apply(lambda row: row["ap"]["dot11.device.last_bssid"], axis=1)
+    wlan_df["ap_bool"] = False
 
     # Attempt to fill in missing APs
     for ap in aps:
-        set1 = set(wlan_df[wlan_df['ap']=='00:00:00:00:00:00']['macaddr'].values)
+        set1 = set(wlan_df[wlan_df["ap"] == "00:00:00:00:00:00"]["macaddr"].values)
         try:
-            list2 = ap['dot11.device']['dot11.device.associated_client_map']
+            list2 = ap["dot11.device"]["dot11.device.associated_client_map"]
         except:
             continue
         common = list(set1.intersection(list2))
         for val in common:
-            ind = wlan_df[wlan_df['macaddr']==val].index[0]
+            ind = wlan_df[wlan_df["macaddr"] == val].index[0]
             # print(ind)
-            wlan_df.at[ind, 'ap'] = ap['kismet.device.base.macaddr']
+            wlan_df.at[ind, "ap"] = ap["kismet.device.base.macaddr"]
 
     # Driver Code
     ssids_df = pd.DataFrame()
     ssids_num = pd.DataFrame()
-    columns = ['ssid', 'macaddr', 'manuf', 'ap_bool']
+    columns = ["ssid", "macaddr", "manuf", "ap_bool"]
 
     for ssid in ssids:
         # ssid device numbers
         ssid_data = {
-            "ssid": [ssid['dot11.ssidgroup.ssid']], 
-            "num_probing": [ssid['dot11.ssidgroup.probing_devices_len']], 
-            "num_responding": [ssid['dot11.ssidgroup.responding_devices_len']]
+            "ssid": [ssid["dot11.ssidgroup.ssid"]],
+            "num_probing": [ssid["dot11.ssidgroup.probing_devices_len"]],
+            "num_responding": [ssid["dot11.ssidgroup.responding_devices_len"]],
         }
-        
+
         ssids_num = pd.concat([ssids_num, pd.DataFrame(ssid_data)], ignore_index=True)
 
         temp_res = pd.DataFrame(columns=columns)
-        if (len(ssid['dot11.ssidgroup.responding_devices']) != 0):
+        if len(ssid["dot11.ssidgroup.responding_devices"]) != 0:
             # responding devices devices nodes/edges
-            temp_res = pd.DataFrame(ssid['dot11.ssidgroup.responding_devices'])
-            temp_res['ssid'] = ssid['dot11.ssidgroup.ssid']
-            temp_res.rename(columns={0: 'devices'}, inplace=True)
-            temp_res = temp_res[['ssid', 'devices']]
-            temp_res['ap_bool'] = True
+            temp_res = pd.DataFrame(ssid["dot11.ssidgroup.responding_devices"])
+            temp_res["ssid"] = ssid["dot11.ssidgroup.ssid"]
+            temp_res.rename(columns={0: "devices"}, inplace=True)
+            temp_res = temp_res[["ssid", "devices"]]
+            temp_res["ap_bool"] = True
             # Updating device keys with mac addresses
-            temp_res = pd.merge(temp_res, aps_df.loc[:, ~aps_df.columns.isin(['ssid'])], left_on='devices', right_on='key', how='inner')
+            temp_res = pd.merge(
+                temp_res, aps_df.loc[:, ~aps_df.columns.isin(["ssid"])], left_on="devices", right_on="key", how="inner"
+            )
             temp_res = temp_res[columns]
-        
+
         temp_prob = pd.DataFrame(columns=columns)
-        if (len(ssid['dot11.ssidgroup.probing_devices']) != 0 and ssid['dot11.ssidgroup.ssid']!='eduroam'):
+        if len(ssid["dot11.ssidgroup.probing_devices"]) != 0 and ssid["dot11.ssidgroup.ssid"] != "eduroam":
             # probing devices nodes/edges
-            temp_prob = pd.DataFrame(ssid['dot11.ssidgroup.probing_devices'])
-            temp_prob['ssid'] = ssid['dot11.ssidgroup.ssid']
-            temp_prob.rename(columns={0: 'devices'}, inplace=True)
-            temp_prob = temp_prob[['ssid', 'devices']]
+            temp_prob = pd.DataFrame(ssid["dot11.ssidgroup.probing_devices"])
+            temp_prob["ssid"] = ssid["dot11.ssidgroup.ssid"]
+            temp_prob.rename(columns={0: "devices"}, inplace=True)
+            temp_prob = temp_prob[["ssid", "devices"]]
             # Updating device keys with mac addresses
-            temp_prob = pd.merge(temp_prob, wlan_df, left_on='devices', right_on='key', how='inner')
+            temp_prob = pd.merge(temp_prob, wlan_df, left_on="devices", right_on="key", how="inner")
             temp_prob = temp_prob[columns]
 
         # concat all
@@ -151,19 +165,19 @@ def create_ssid_graph(sub_path):
     # ssids_df = pd.concat([ssids_df, wlan_df], ignore_index=True)
 
     # Remove Aruba Networks HP devices
-    wlan_df = wlan_df[wlan_df['manuf']!='Aruba Networks HP']
+    wlan_df = wlan_df[wlan_df["manuf"] != "Aruba Networks HP"]
     # Remove empty devices with empty APs
-    wlan_df = wlan_df[wlan_df['ap']!='00:00:00:00:00:00']
+    wlan_df = wlan_df[wlan_df["ap"] != "00:00:00:00:00:00"]
 
-    wlan_df_sample = wlan_df.sample(n=min(len(wlan_df),1000))
+    wlan_df_sample = wlan_df.sample(n=min(len(wlan_df), 1000))
 
     # Graph Setup
     net = Network(
         # notebook=True,
         # cdn_resources="remote",
-        height="1080px", 
-        width="100%", 
-        bgcolor="#222222", 
+        height="1080px",
+        width="100%",
+        bgcolor="#222222",
         font_color="white",
         # select_menu=True,
         # filter_menu=True,
@@ -172,17 +186,18 @@ def create_ssid_graph(sub_path):
     # Physics
     net.force_atlas_2based()
 
-    images = {'Intel Corporation':'https://raw.githubusercontent.com/anrath/data/master/intel_logo.jpg', 
-            'Apple':'https://raw.githubusercontent.com/anrath/data/master/apple_gray_logo.png', 
-            'Aruba Networks HP':'https://raw.githubusercontent.com/anrath/data/master/aruba_logo.jpg'
-            }
+    images = {
+        "Intel Corporation": "https://raw.githubusercontent.com/anrath/data/master/intel_logo.jpg",
+        "Apple": "https://raw.githubusercontent.com/anrath/data/master/apple_gray_logo.png",
+        "Aruba Networks HP": "https://raw.githubusercontent.com/anrath/data/master/aruba_logo.jpg",
+    }
 
     # SSID-AP Edges
 
-    sources = ssids_df['ssid']
-    targets = ssids_df['macaddr']
-    manuf = ssids_df['manuf']
-    ap_bool = ssids_df['ap_bool']
+    sources = ssids_df["ssid"]
+    targets = ssids_df["macaddr"]
+    manuf = ssids_df["manuf"]
+    ap_bool = ssids_df["ap_bool"]
 
     edge_data = zip(sources, targets, manuf, ap_bool)
 
@@ -192,20 +207,36 @@ def create_ssid_graph(sub_path):
         manuf = e[2]
         ap_bool = e[3]
 
-        net.add_node(src, src, title=src, group=1, shape='diamond')
+        net.add_node(src, src, title=src, group=1, shape="diamond")
 
         device_group = 2 if ap_bool else 3
 
-        if (manuf in images.keys()):
-            net.add_node(dst, dst, title=dst, group=device_group, shape='circularImage', image=images[manuf], brokenImage='circle')
+        if manuf in images.keys():
+            net.add_node(
+                dst,
+                dst,
+                title=dst,
+                group=device_group,
+                shape="circularImage",
+                image=images[manuf],
+                brokenImage="circle",
+            )
         else:
-            net.add_node(dst, dst, title=dst, group=device_group, shape='circularImage', image='https://raw.githubusercontent.com/anrath/data/master/blank.png', brokenImage='circle')
+            net.add_node(
+                dst,
+                dst,
+                title=dst,
+                group=device_group,
+                shape="circularImage",
+                image="https://raw.githubusercontent.com/anrath/data/master/blank.png",
+                brokenImage="circle",
+            )
         net.add_edge(src, dst)
 
     # AP-Device Edges
-    aps = wlan_df_sample['ap']
-    devices = wlan_df_sample['macaddr']
-    manuf = wlan_df_sample['manuf']
+    aps = wlan_df_sample["ap"]
+    devices = wlan_df_sample["macaddr"]
+    manuf = wlan_df_sample["manuf"]
     # ap_bool = wlan_df['ap_bool']
 
     edge_data = zip(aps, devices, manuf)
@@ -216,14 +247,38 @@ def create_ssid_graph(sub_path):
         manuf = e[2]
 
         # net.add_node(src, src, title=src, group=2, shape='diamond')
-        net.add_node(src, src, title=src, group=2, shape='circularImage', image=images['Aruba Networks HP'], brokenImage='circle')
+        net.add_node(
+            src,
+            src,
+            title=src,
+            group=2,
+            shape="circularImage",
+            image=images["Aruba Networks HP"],
+            brokenImage="circle",
+        )
 
         device_group = 3
 
-        if (manuf in images.keys()):
-            net.add_node(dst, dst, title=dst, group=device_group, shape='circularImage', image=images[manuf], brokenImage='circle')
+        if manuf in images.keys():
+            net.add_node(
+                dst,
+                dst,
+                title=dst,
+                group=device_group,
+                shape="circularImage",
+                image=images[manuf],
+                brokenImage="circle",
+            )
         else:
-            net.add_node(dst, dst, title=dst, group=device_group, shape='circularImage', image='https://raw.githubusercontent.com/anrath/data/master/blank.png', brokenImage='circle')
+            net.add_node(
+                dst,
+                dst,
+                title=dst,
+                group=device_group,
+                shape="circularImage",
+                image="https://raw.githubusercontent.com/anrath/data/master/blank.png",
+                brokenImage="circle",
+            )
         net.add_edge(src, dst)
 
     # Hover Text
@@ -237,8 +292,8 @@ def create_ssid_graph(sub_path):
     net.write_html(name=f"{HTML_PATH}/ssid_{sub_path}.html", notebook=False, local=True)
 
 
-DATA_PATH = './data'
-DATA_SUB_PATH = ['campus', 'flats']
-HTML_PATH = './apps/templates/home'
+DATA_PATH = "./data"
+DATA_SUB_PATH = ["campus", "flats"]
+HTML_PATH = "./apps/templates/home"
 
 ssid_dict = {}
